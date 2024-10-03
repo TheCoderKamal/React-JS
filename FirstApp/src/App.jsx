d130f91dc1999470d79bfa169790155e6a4f24b2
TheCoderKamal <kamalsagpariya7@gmail.com>
2024-10-03 19:49:22 +0530

    commit

--- FirstApp/src/App.jsx
+++ FirstApp/src/App.jsx
@@ -6,6 +6,11 @@ import './App.css'
 function App() {
   let arr = [1, "kamal", true];
   let arr2 = [...arr, "shreya", "jay"];
+  let arr3 = [
+    { name: "Kamal", age: 25 },
+    { name: "Shreya", age: 28 },
+    { name: "Jay", age: 30 },
+  ]
 
   return (
     <>
@@ -26,6 +31,19 @@ function App() {
           return (
             <div key={index} style={{border: "1px solid black"}}  >
               <p>{item}</p>
+            </div>
+          )
+        })
+      }
+
+      <br />
+
+      {
+        arr3.map((item, index) => {
+          return (
+            <div key={index} style={{border: "1px solid black"}}  >
+              <p>{item.name}</p>
+              <p>{item.age}</p>
             </div>
           )
         })
